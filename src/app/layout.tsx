3c260a212597144581c4d4a9674be526f63778f8
Alex Yuan <alex@alexyuan.me>
2023-05-10 00:29:11 -0400

    use app directory RSC

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+// These styles apply to every route in the application
+import Link from "next/link";
+import "../styles/globals.css";
+
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata = {
+  title: "Hacker News Reader",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.className}>
+      <body>
+        <div className="bg-white sm:mx-20 sm:mt-3 md:mx-40 md:mt-5">
+          <nav className="flex justify-between bg-orange-500 px-5 py-2">
+            <Link href="/">
+              <p className="text-md font-semibold">Hacker News Reader</p>
+            </Link>
+          </nav>
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
+const ITEMS_PER_PAGE = 30;
+
+export default async function HomePage({ searchParams }) {
+  let page: number;
+  if (searchParams.p) {
+    page = parseInt(String(searchParams.p));
+  } else {
+    page = 1;
+  }
+
+  let URL: string = `${process.env.HACKER_NEWS_API_ENDPOINT}/v0/topstories.json`;
+  const res = await fetch(URL);
+  const topStoriesIds: number[] = await res.json();
+  const pointer: number = ITEMS_PER_PAGE * (page - 1);
+  const topStories: any[] = await Promise.all(
+    topStoriesIds.slice(pointer, pointer + ITEMS_PER_PAGE).map(async (id) => {
+      const res = await fetch(
+        `${process.env.HACKER_NEWS_API_ENDPOINT}/v0/item/${id}.json`
+      );
+      const story = await res.json();
+      return story;
+    })
+  );
+
+  return (
+    <>
+      <div className="flex flex-col space-y-4 p-5">
+        {topStories.map((story) => (
+          <div key={story.id} className="flex space-x-4">
+            <div className="flex w-12 shrink-0 items-center justify-center bg-gray-100 text-sm">
+              {story.score}
+            </div>
+            <div>
+              <a href={story.url}>
+                <div>
+                  {story.title}{" "}
+                  {story.url && (
+                    <span className="text-sm text-gray-600">
+                      ({story.url.match(/^https?:\/\/([^/?#]+)(?:[/?#]|$)/i)[1]}
+                      )
+                    </span>
+                  )}
+                </div>
+              </a>
+              <div className="text-sm text-gray-600">
+                Posted by{" "}
+                <Link className="hover:underline" href={`/user?id=${story.by}`}>
+                  {story.by}
+                </Link>{" "}
+                {dayjs.unix(story.time).fromNow()} |{" "}
+                <Link className="hover:underline" href={`/item?id=${story.id}`}>
+                  {story.descendants || 0} comments
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Link className="ml-20 mt-4" href={`/page?p=${page + 1}`}>
+        More
+      </Link>
+      <p className="py-3 text-center text-xs">
+        Hacker News belongs to Y Combinator.
+      </p>
+    </>
+  );
+}

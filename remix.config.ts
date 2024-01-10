436d4ddf3dabf6515477c425abc62009774a69df
Alex Yuan <alex@alexyuan.me>
2024-01-09 19:21:38 -0500

    finally fix it by using isbot@3

--- remix.config.ts
+++ remix.config.ts
@@ -1,14 +1,3 @@
 export default {
-  future: {
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
   ignoredRouteFiles: ["**/.*"],
-  tailwind: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
 } satisfies import('@remix-run/dev').AppConfig;

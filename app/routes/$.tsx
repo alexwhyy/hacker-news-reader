19b7b56f6da32fafc97c4a69b8616f2aea3ff08b
Alex Yuan <alex@alexyuan.me>
2024-12-05 21:23:23 -0500

    format

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -3,8 +3,10 @@ export function loader() {
 }
 
 export default function NotFoundPage() {
-  return <div className="p-5">
-    <h1>404 - Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
-  </div>;
+  return (
+    <div className="p-5">
+      <h1>404 - Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
+  );
 }

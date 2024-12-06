5f7926c981190362728e3b9f37a88bd7fe5534d0
Alex Yuan <alex@alexyuan.me>
2024-12-05 21:22:16 -0500

    add 404

---
+++ app/routes/$.tsx
@@ -0,0 +1,10 @@
+export function loader() {
+  return new Response("Not Found", { status: 404 });
+}
+
+export default function NotFoundPage() {
+  return <div className="p-5">
+    <h1>404 - Not Found</h1>
+    <p>The page you are looking for does not exist.</p>
+  </div>;
+}
